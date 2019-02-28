b208964812c9d25580cc8824e365f0220eaa49ed
whrfriends <whrfriends@163.com>
2019-02-28 14:54:44 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+class main():
+    """
+    main entrence
+    paragrams :
+    None
+    """
+    def __init__(self):
+        self.randate =np.random.rand(3,3)
+        print(self.randate)
+
+if __name__ == "__main__":
+    demo1 = main()
+
---
+++ slam.py
@@ -0,0 +1,14 @@
+class slam:
+    
+    def __init__(self,parent=None):
+        
+        self.name = "wuhuarui"
+
+
+    def sayhello(self):
+        print(self.name+" say hello to you")
+
+if __name__ == "__main__":
+    slam1 =  slam()
+    slam1.sayhello()
+
